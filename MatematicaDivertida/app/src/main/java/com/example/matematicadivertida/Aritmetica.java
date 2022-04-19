28e28c4c5ba90d3b0f60b90598ad02fcd0090273
Jéssica Nascimento <59929856+jessgn@users.noreply.github.com>
2022-04-18 22:46:19 -0300

    update

--- MatematicaDivertida/app/src/main/java/com/example/matematicadivertida/Aritmetica.java
+++ MatematicaDivertida/app/src/main/java/com/example/matematicadivertida/Aritmetica.java
@@ -3,6 +3,7 @@ package com.example.matematicadivertida;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -16,7 +17,7 @@ public class Aritmetica extends AppCompatActivity {
     Random r = new Random();
     String[] operandos;
     int qtd =0;
-    int aux = 1;
+    int aux = 0;
     int pontos= 0;
     int num1, num2, result =0;
     String operando;
@@ -27,24 +28,23 @@ public class Aritmetica extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aritmetica);
+        operandos= new String[]{ "+", "-"};
+        TextView inputA = findViewById(R.id.textViewOutputAri);
+        num1 = r.nextInt(10);
+        num2 = r.nextInt(10);
+        operando = operandos[r.nextInt(2)];
+
+        if (operando == "+"){
+            result = num1 + num2;
+        }else {
+            result = num1 - num2;
+        }
+        inputA.setText(String.valueOf(num1) + operando + String.valueOf(num2));
     }
 
     public void nextAri(View view){
+        String message;
         if(aux < 5){
-            operandos= new String[]{ "+", "-" };
-            TextView inputA = findViewById(R.id.textViewOutputAri);
-            num1 = r.nextInt(10);
-            num2 = r.nextInt(10);
-            operando = operandos[r.nextInt(1)];
-            String message;
-
-            if (operando == "+"){
-                result = num1 + num2;
-            }else {
-                result = num1 - num2;
-            }
-            inputA.setText(String.valueOf(num1) + operando + String.valueOf(num2));
-
             EditText respostaIn = findViewById(R.id.editTextNumberAri);
              if (respostaIn.getText().length() < 0){
                  Toast.makeText(this, "Insira uma resposta válida", Toast.LENGTH_LONG).show();
@@ -52,6 +52,7 @@ public class Aritmetica extends AppCompatActivity {
                  int resposUsu = Integer.parseInt(respostaIn.getText().toString());
                   if(resposUsu == result){
                       message = "A resposta está correta!";
+                      pontos += 20;
                   }else{
                       message = "A resposta está incorreta! \n A resposta certa é " + result;
                   }
@@ -59,7 +60,31 @@ public class Aritmetica extends AppCompatActivity {
                          .setTitle("Resposta")
                          .setMessage(message)
                          .show();
+                  aux++;
+
+                 operandos= new String[]{ "+", "-" };
+                 TextView inputA = findViewById(R.id.textViewOutputAri);
+                 num1 = r.nextInt(10);
+                 num2 = r.nextInt(10);
+                 int op = r.nextInt(2);
+                 operando = operandos[op];
+
+                 if (operando == "+"){
+                     result = num1 + num2;
+                 }else {
+                     result = num1 - num2;
+                 }
+                 inputA.setText(String.valueOf(num1) + operando + String.valueOf(num2));
+                 respostaIn.setText("");
              }
+        }else{
+            new AlertDialog.Builder(this)
+                    .setTitle("Fim de jogo!")
+                    .setMessage("A pontuação é " + pontos)
+                    .show();
+            Intent it = new Intent(this, MainActivity.class);
+            startActivity(it);
+            finish();
         }
     }
 }
